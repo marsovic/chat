f156c97d5d72bb3b9f9fc7fe32b6b2dd381053e0
Marko Arsovic <marko.arsovic@edu.ece.fr>
2020-06-01 14:06:30 +0200

    adding some files

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
+1
+
+<?php //-- Very simple variant
+$useragent = $_SERVER['HTTP_USER_AGENT']; 
+$iPod = stripos($useragent, "iPod"); 
+$iPad = stripos($useragent, "iPad"); 
+$iPhone = stripos($useragent, "iPhone");
+$Android = stripos($useragent, "Android"); 
+$iOS = stripos($useragent, "iOS");
+//-- You can add billion devices 
+
+$DEVICE = ($iPod||$iPad||$iPhone||$Android||$iOS||$webOS||$Blackberry||$IEMobile||$OperaMini);
+
+if ($DEVICE !=true) {?>
+
 <?php header( 'Location: /index.html' ) ;  ?>
+
+<?php }else{ ?> 
+
+<html> <h1> Essayez de l'utiliser sur un PC !</h1> </html>
+<!-- What you want for all mobile devices. Anything with all HTML codes --> 
+<?php } ?>
